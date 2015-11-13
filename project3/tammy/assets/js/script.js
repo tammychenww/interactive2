473fe46ef944e658effea7e20ef27ccd9e8808d2
tammychenww <wchen@cca.edu>
2015-11-13 15:53:39 -0800

    a
    a

---
+++ project3/tammy/assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('.light').click(function(){
+    $('body').css('background', '#ED1A4C');
+  });
+
+  $('.light').clicked(function(){
+    $('body').css('background', '#ED1A9C');
+  });
+
+});
+
